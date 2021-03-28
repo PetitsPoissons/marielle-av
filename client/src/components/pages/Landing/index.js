8b48bc34cc85d51910c60cbc0dec809f20655572
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-03-27 17:34:25 -0700

    one more file

---
+++ client/src/components/pages/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../layoutComponents/Navbar';
+import { LandingContainer } from './LandingElements';
+
+const Landing = () => {
+  return (
+    <>
+      <LandingContainer>
+        <Navbar />
+      </LandingContainer>
+    </>
+  );
+};
+export default Landing;
