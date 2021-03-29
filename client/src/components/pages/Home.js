3e16cfa098b610fc906d0151f44a33f7e8a9ba55
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-03-29 03:56:36 -0700

    react-materialize

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,46 +1,41 @@
 import React from 'react';
-import Parallax from 'react-rellax';
+// import M from 'materialize-css';
+import { Parallax } from 'react-materialize';
 import image1 from '../../assets/pexels-bulbfish-1143754.jpeg';
 import image2 from '../../assets/pexels-jatuphon-buraphon-348689.jpeg';
 import image3 from '../../assets/pexels-maria-orlova-4947354.jpeg';
 import logo from '../../assets/pexels-vegan-liftz-2377164.jpeg';
 
 const Home = () => {
+  // useEffect(() => {
+  //   let elements = document.querySelectorAll('.parallax');
+  //   M.Parallax.init(elements);
+  // }, []);
   return (
-    <>
-      <div className="container container-home">
-        <div className="row row-one">
-          <div className="col s6 row-one-left">
-            <Parallax speed={-5}>
-              <img src={image1} alt="Vegan Food" className="" />
-            </Parallax>
-          </div>
-          <div className="col s6 row-one-right">
-            <Parallax speed={-5}>
-              <img src={image2} alt="Lettuce Fiels" className="" />
-            </Parallax>
-          </div>
-        </div>
-        <div className="row row-two">
-          <div className="col s4 white row-two-left">
-            <h2>Test Parallax</h2>
-            <p>Demo for materialize css parallax</p>
-          </div>
-
-          <div className="col s4 row-two-center">
-            <img src={logo} alt="Lettuce Fiels" className="" />
-          </div>
-          <div className="col s4 row-two-right">
-            <img src={image3} alt="Lettuce Fiels" className="" />
-          </div>
+    <div>
+      <Parallax
+        image={<img alt="" src={image1} />}
+        options={{
+          responsiveThreshold: 5,
+        }}
+      />
+      <div className="section white">
+        <div className="row container">
+          <h2 className="header">Parallax</h2>
+          <p className="grey-text text-darken-3 lighten-3">
+            Parallax is an effect where the background content or image in this
+            case, is moved at a different speed than the foreground content
+            while scrolling.
+          </p>
         </div>
       </div>
-      <div className="container container-home">
-        <div className="row row-three grey white-text">
-          <h3>End of demo</h3>
-        </div>
-      </div>
-    </>
+      <Parallax
+        image={<img alt="" src={image2} />}
+        options={{
+          responsiveThreshold: 0,
+        }}
+      />
+    </div>
   );
 };
 export default Home;
